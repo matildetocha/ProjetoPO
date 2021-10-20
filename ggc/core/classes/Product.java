c57b884227e9230222e98649cd88c773e299f152
GLima727 <guilhermegutierrez727@gmai.com>
2021-10-20 17:21:51 +0100

    adicionar metodos em Produto

--- ggc/core/classes/Product.java
+++ ggc/core/classes/Product.java
@@ -3,11 +3,17 @@ package ggc.core.classes;
 abstract class Product{
     
     private double _maxPrice;
-    private String id;
+    private String _id;
 
     public void Product(String id){}
 
    // abstract protected void checkQuantity(int quantity, Partner p);
+    public String getId(){
+        return _id;
+    }
 
+    public double getMaxPrice(){
+        return _maxPrice;
+    }
 
 }
