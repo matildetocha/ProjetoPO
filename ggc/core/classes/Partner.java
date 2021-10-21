2ea201d4323f9665641850a4d28157efcf778963
GLima727 <guilhermegutierrez727@gmail.com>
2021-10-21 13:58:07 +0100

    criei o toString no parceiro e adicionei instancias ao construtor

--- ggc/core/classes/Partner.java
+++ ggc/core/classes/Partner.java
@@ -10,11 +10,13 @@ public class Partner {
     private double _valueSales;
     private double _valuePaidSales;
         
-    public Partner(String name, String adress){
+    public Partner(String name, String id, String adress){
         _name = name;
+        _id = id;
         _adress = adress;
         // _status = Normal;
         _points = 0;
+        _valueAcquisitions = _valueSales = _valuePaidSales = 0;
     }
 
     public String getName(){
@@ -24,4 +26,9 @@ public class Partner {
     public String getId(){
         return _id;
     }
-}
+
+    public String toString(){
+
+        return _id + "|" + _name + "|" + _adress + "|" + _status + "|" +  _points + "|" + _valueAcquisitions + "|" + _valueSales + "|" + _valuePaidSales; 
+    }
+}   
