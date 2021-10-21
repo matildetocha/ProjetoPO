83626129e4db9f6fea801c1d570ecce9e8e1a817
GLima727 <guilhermegutierrez727@gmail.com>
2021-10-21 13:56:57 +0100

    Fiz o registrar parceiro e mostrar parceiros sem excecoes

--- ggc/app/partners/DoRegisterPartner.java
+++ ggc/app/partners/DoRegisterPartner.java
@@ -4,6 +4,8 @@ import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.core.WarehouseManager;
 //FIXME import classes
+import ggc.core.classes.Partner;
+
 
 /**
  * Register new partner.
@@ -13,11 +15,16 @@ class DoRegisterPartner extends Command<WarehouseManager> {
   DoRegisterPartner(WarehouseManager receiver) {
     super(Label.REGISTER_PARTNER, receiver);
     //FIXME add command fields
+    addStringField("Id", "Qual o id do Parceiro?: ");
+    addStringField("Nome", "Qual o nome do Parceiro?: ");
+    addStringField("Morada", "Qual a morada do Parceiro?: ");
   }
 
   @Override
   public void execute() throws CommandException {
     //FIXME implement command
+    Partner partner = new Partner(stringField("Nome"),stringField("Id"),stringField("Morada"));
+    _receiver.registerPartner(partner);
   }
 
 }
--- ggc/app/partners/DoShowAllPartners.java
+++ ggc/app/partners/DoShowAllPartners.java
@@ -3,6 +3,10 @@ package ggc.app.partners;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 import ggc.core.WarehouseManager;
+import ggc.core.classes.Partner;
+
+import java.util.List;
+import java.util.ArrayList;
 //FIXME import classes
 
 /**
@@ -12,11 +16,13 @@ class DoShowAllPartners extends Command<WarehouseManager> {
 
   DoShowAllPartners(WarehouseManager receiver) {
     super(Label.SHOW_ALL_PARTNERS, receiver);
+
   }
 
   @Override
   public void execute() throws CommandException {
     //FIXME implement command
+    _receiver.displayPartners();
   }
 
 }
