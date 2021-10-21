3eca4fd0f3c113b7b026fb31a1a933ffd5c75e0e
GLima727 <guilhermegutierrez727@gmai.com>
2021-10-21 13:01:45 +0100

    Adicionar a excecao de avancar tempo negativo ! !

--- ggc/app/main/DoAdvanceDate.java
+++ ggc/app/main/DoAdvanceDate.java
@@ -2,9 +2,12 @@ package ggc.app.main;
 
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
+import ggc.app.exception.InvalidDateException;
 import ggc.core.WarehouseManager;
 import ggc.core.classes.Date;
 //FIXME import classes
+import ggc.core.exception.BadEntryException;
+
 
 /**
  * Advance current date.
@@ -18,9 +21,16 @@ class DoAdvanceDate extends Command<WarehouseManager> {
   }
 
   @Override
-  protected final void execute() throws CommandException {
+  protected final void execute() throws CommandException, InvalidDateException {
+
+    try{
 
     Date.add(integerField("timeAdd"));
+
+    }catch (BadEntryException e){
+
+      throw new InvalidDateException(integerField("timeAdd"));
+    }
     
   }
 
--- ggc/core/classes/Date.java
+++ ggc/core/classes/Date.java
@@ -2,15 +2,22 @@ package ggc.core.classes;
 
 import java.lang.Math;
 
+import ggc.app.exception.InvalidDateException;
+import ggc.core.exception.BadEntryException;
+
 public class Date {
     private static int _days;
 
-    public static void add(int days){
+    public static void add(int days) throws BadEntryException{
+        if(days < 0){ 
+            throw new BadEntryException("");
+
+        }
         _days += days;
     }
     public int difference(Date other){
         //return Math.abs(other.now() - _days);
-        return Math.abs(Date.now()- other.now());
+        return Math.abs(Date.now() - other.now());
     }
 
     public static int now(){
