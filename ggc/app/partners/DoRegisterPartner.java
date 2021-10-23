e7ed56247c010f93be4dce3e0a95f76eda016093
GLima727 <guilhermegutierrez727@gmail.com>
2021-10-23 22:10:36 +0100

    register partner bom

--- ggc/app/partners/DoRegisterPartner.java
+++ ggc/app/partners/DoRegisterPartner.java
@@ -2,12 +2,12 @@ package ggc.app.partners;
 
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-import ggc.app.exception.DuplicatePartnerKeyException;
-import ggc.core.WarehouseManager;
-//FIXME import classes
-import ggc.core.Partner;
-import ggc.core.exception.BadEntryException;
 
+import ggc.app.partners.Message;
+import ggc.app.exception.DuplicatePartnerKeyException;
+
+import ggc.core.WarehouseManager;
+import ggc.core.exception.BadEntryException;
 
 /**
  * Register new partner.
@@ -16,25 +16,19 @@ class DoRegisterPartner extends Command<WarehouseManager> {
 
   DoRegisterPartner(WarehouseManager receiver) {
     super(Label.REGISTER_PARTNER, receiver);
-    //FIXME add command fields
-    addStringField("Id", "Qual o id do Parceiro?: ");
-    addStringField("Nome", "Qual o nome do Parceiro?: ");
-    addStringField("Morada", "Qual a morada do Parceiro?: ");
+
+    addStringField("id", Message.requestPartnerKey());
+    addStringField("name", Message.requestPartnerName());
+    addStringField("address", Message.requestPartnerAddress());
   }
 
   @Override
   public void execute() throws CommandException, DuplicatePartnerKeyException {
-    //FIXME implement command
-    //nao sei se criar o parceiro assim é muito bom
-    try{
+    try {
+      _receiver.registerPartner(stringField("name"), stringField("id"), stringField("address"));
+    } catch (BadEntryException e) {
 
-
-    Partner partner = new Partner(stringField("Nome"),stringField("Id"),stringField("Morada"));
-    _receiver.registerPartner(partner);
-    }catch(BadEntryException e){
-
-      throw new DuplicatePartnerKeyException(stringField("Id"));
+      throw new DuplicatePartnerKeyException(stringField("id"));
     }
   }
-
 }
