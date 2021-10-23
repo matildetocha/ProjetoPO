331841b264d8b4c9ff9f339758ee857de975f395
GLima727 <guilhermegutierrez727@gmail.com>
2021-10-23 21:18:55 +0100

    batch to string and batch constructor, alsoo duvidas em como e q vamos organizar os produtos em batches :O

--- ggc/core/Batch.java
+++ ggc/core/Batch.java
@@ -7,13 +7,19 @@ import ggc.core.Product;
 public class Batch {
     private double _price;
     private int _quantity;
-    private List<Product> _products;
+    private Product _product; //wtf isto vai ser uma lista de produtos ou nao? e q qnd criamos produtos so é para registrar um e metê-lo no batch, idk 
+    private Partner _partner;
 
-    Batch() {
-
+    Batch(Product product, Partner partner, double price, int quantity) {
+        _product = product;
+        _partner = partner;
+        _price = price;
+        _quantity = quantity;
     }
 
-    //public String toString(){}
+    public String toString(){
+        return _partner.getId() + "|" + _product.getId() + "|" + _price + "|" + _quantity;
+    }
 
     //Batch makeCopy(){}
     
