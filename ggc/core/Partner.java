6426f8ea54f3fd76055d1651a10effe2abb8316c
matildetocha <matilde.tocha@tecnico.ulisboa.pt>
2021-10-24 02:04:33 +0100

    método set status

--- ggc/core/Partner.java
+++ ggc/core/Partner.java
@@ -1,34 +1,42 @@
 package ggc.core;
 
 public class Partner {
+    private static final String NORMAL = "Normal";
+    private static final String SELECTION = "Selection";
+    private static final String ELITE = "Elite";
+
     private String _id;
     private String _name;
-    private String _adress;
+    private String _address;
     private String _status;
     private double _points;
     private double _valueAcquisitions;
     private double _valueSales;
     private double _valuePaidSales;
         
-    public Partner(String name, String id, String adress){
+    Partner(String name, String id, String address){
         _name = name;
         _id = id;
-        _adress = adress;
-        // _status = Normal;
+        _address = address;
+        _status = NORMAL;
         _points = 0;
         _valueAcquisitions = _valueSales = _valuePaidSales = 0;
     }
 
-    public String getName(){
-        return _name;
+    String getId(){
+        return _id;
     }
 
-    public String getId(){
-        return _id;
+    void setStatus() {
+        if (_points <= 2000) _status = NORMAL;
+        else if (_points < 25000 && _points > 2000) _status = SELECTION;
+        else if (_points > 25000) _status = ELITE;
+
     }
 
     public String toString(){
 
-        return _id + "|" + _name + "|" + _adress + "|" + _status + "|" +  _points + "|" + _valueAcquisitions + "|" + _valueSales + "|" + _valuePaidSales; 
+        return _id + "|" + _name + "|" + _address + "|" + _status + "|" +  _points + 
+            "|" + _valueAcquisitions + "|" + _valueSales + "|" + _valuePaidSales; 
     }
 }   
