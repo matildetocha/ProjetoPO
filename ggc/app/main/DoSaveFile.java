3fc078662021fafd57a5006efaf7033094ff5dd6
GLima727 <guilhermegutierrez727@gmail.com>
2021-10-28 15:51:32 +0100

    92%

--- ggc/app/main/DoSaveFile.java
+++ ggc/app/main/DoSaveFile.java
@@ -1,56 +1,40 @@
 package ggc.app.main;
 
-import pt.tecnico.uilib.menus.Command;
-import pt.tecnico.uilib.menus.CommandException;
-
 import ggc.app.exception.FileOpenFailedException;
 import ggc.core.WarehouseManager;
 import ggc.core.exception.MissingFileAssociationException;
-
-import java.io.FileNotFoundException;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.FileWriter;
-import java.io.FileReader;
+import pt.tecnico.uilib.menus.Command;
+import pt.tecnico.uilib.menus.CommandException;
+import pt.tecnico.uilib.forms.Form;
 
 /**
  * Save current state to file under current name (if unnamed, query for name).
  */
 class DoSaveFile extends Command<WarehouseManager> {
-
   /** @param receiver */
   DoSaveFile(WarehouseManager receiver) {
     super(Label.SAVE, receiver);
-    if ((_receiver.getFilename()).equals(""))
-    {
-    addStringField("filename", Message.newSaveAs());
-  }
-  
   }
 
   @Override
   public final void execute() throws CommandException {
     try {
- 
-      if ((_receiver.getFilename()).equals(""))
-        {
-        _receiver.saveAs(stringField("filename"));
+      if (_receiver.getFilename().equals("")) {
+        
+        Form form = new Form("File");
+        form.addStringField("filename", Message.newSaveAs());
+        form.parse();
+        _receiver.saveAs(form.stringField("filename"));
       }
       else
-      _receiver.save();
-    } catch (MissingFileAssociationException e) {
-      e.printStackTrace();
-    }catch (FileNotFoundException e){
-      e.printStackTrace();
-      }catch (IOException e) {
-        e.printStackTrace();
-
+        _receiver.save();
     }
-
-
+    catch (MissingFileAssociationException e) {
+      e.printStackTrace();
+    }
+    catch (IOException e) {
+      throw new FileOpenFailedException(_receiver.getFilename());
+    }
   }
-
 }
