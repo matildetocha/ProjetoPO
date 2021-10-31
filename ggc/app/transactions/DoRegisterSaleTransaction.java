641080dd213189f6925787073cd02f31494672ae
GLima727 <guilhermegutierrez727@gmail.com>
2021-10-31 01:27:41 +0100

    do register sale transaction

--- ggc/app/transactions/DoRegisterSaleTransaction.java
+++ ggc/app/transactions/DoRegisterSaleTransaction.java
@@ -2,6 +2,7 @@ package ggc.app.transactions;
 
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
+import ggc.app.exception.UnavailableProductException;
 import ggc.core.WarehouseManager;
 //FIXME import classes
 
@@ -13,11 +14,20 @@ public class DoRegisterSaleTransaction extends Command<WarehouseManager> {
   public DoRegisterSaleTransaction(WarehouseManager receiver) {
     super(Label.REGISTER_SALE_TRANSACTION, receiver);
     //FIXME maybe add command fields 
+    addStringField("partnerId", Message.requestPartnerKey());
+    addIntegerField("deadline", Message.requestPaymentDeadline());
+    addStringField("productId", Message.requestProductKey());
+    addIntegerField("quantity", Message.requestAmount());
   }
 
   @Override
   public final void execute() throws CommandException {
     //FIXME implement command
+    try{
+      _receiver.registerSale(integerField("quantity"), stringField("productId"), stringField("partnerId"), integerField("deadline"));
+    }catch(UnavailableProductException e){
+      throw new UnavailableProductException(stringField("productId"), integerField("quantity"), _receiver.getAvailableStock(stringField("productId")));
+    }
   }
 
 }
